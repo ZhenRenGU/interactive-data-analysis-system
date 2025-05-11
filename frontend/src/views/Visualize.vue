b826c505af3d953a0166806a7c4e9fb4696ffeaf
OldMonth <2072229572@qq.com>
2025-05-11 14:18:41 +0800

    Component-initialization

---
+++ frontend/src/views/Visualize.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="visualize-container">
+    <el-card class="box-card">
+      <template #header>
+        <div class="clearfix">
+          <span>数据可视化</span>
+        </div>
+      </template>
+      <div class="content">
+        <p>正在开发数据可视化功能，敬请期待...</p>
+        <el-button type="primary" @click="goBack">返回</el-button>
+      </div>
+    </el-card>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Visualize',
+  data() {
+    return {
+      filename: this.$route.params.filename
+    }
+  },
+  methods: {
+    goBack() {
+      this.$router.push('/preview/' + this.filename)
+    }
+  }
+}
+</script>
+
+<style scoped>
+.visualize-container {
+  max-width: 1200px;
+  margin: 20px auto;
+  padding: 0 20px;
+}
+.content {
+  min-height: 300px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+</style>
